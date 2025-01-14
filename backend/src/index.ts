9f08a65a9e9014ed334e6f411ac42b8e37d7810e
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2025-01-14 20:43:02 -0300

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import Fastify from "fastify"
+
+const server = Fastify()
+server.get("/", async (_request, _reply) => {
+  return { message: "Hello! Fastify with TypeScript!" }
+})
+const start = async () : Promise<void> => {
+  try {
+    await server.listen({ port: 3000 })
+    console.log("Server listening on http://localhost:3000 ")
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+
+start().then().catch(error => console.error(error))
