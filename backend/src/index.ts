ea6475d09f906c348a86369bbf746fa5625e5196
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2025-01-14 20:46:43 -0300

    linting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import Fastify from 'fastify'
 
 const server = Fastify()
+
 server.get('/', async (_request, _reply) => {
   return { message: 'Hello! Fastify with TypeScript!' }
 })
+
 const start = async (): Promise<void> => {
   try {
     await server.listen({ port: 3000 })
