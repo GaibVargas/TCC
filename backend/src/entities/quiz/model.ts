07a9147aad74167c0b49bc5098d3d112fb012b94
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2025-01-31 21:37:24 -0300

    feat: retira desabilitações de warnings do ts

--- backend/src/entities/quiz/model.ts
+++ backend/src/entities/quiz/model.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import prisma from '../../config/db'
 import HttpRequestError from '../../utils/error'
 import userModel from '../user/model'
