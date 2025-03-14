882320052c3789abf95f470634ecc8b450f59f7c
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2025-03-14 15:01:04 -0300

    feat: remove criação de quiz base

--- backend/src/entities/session/sessions-manager.ts
+++ backend/src/entities/session/sessions-manager.ts
@@ -1,92 +1,14 @@
-import { QuestionType, Quiz } from '../quiz/type'
-import { MinUser, UserRoles } from '../user/type'
+import { Quiz } from '../quiz/type'
+import { MinUser } from '../user/type'
 import { Session } from './session'
 import { CustomSocket } from '../../socket/types'
-
-const baseQuiz: Quiz = {
-  public_id: 'quiz-id',
-  title: 'Quiz teste',
-  questions: [
-    {
-      id: 1,
-      public_id: 'quiz-question-1',
-      description: 'Questão 1 multi choice',
-      correct_text_answer: '',
-      time_limit: 60,
-      type: QuestionType.MULTI_CHOICE,
-      options: [
-        {
-          public_id: 'option-1',
-          description: 'Primeira opção',
-          is_correct_answer: true,
-        },
-        {
-          public_id: 'option-2',
-          description: 'Segunda opção',
-          is_correct_answer: false,
-        },
-        {
-          public_id: 'option-3',
-          description: 'Terceira opção',
-          is_correct_answer: false,
-        },
-        {
-          public_id: 'option-4',
-          description: 'Quarta opção',
-          is_correct_answer: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      public_id: 'quiz-question-2',
-      description: 'Questão 2 true false',
-      correct_text_answer: '',
-      time_limit: null,
-      type: QuestionType.TRUE_OR_FALSE,
-      options: [
-        {
-          public_id: 'true-1',
-          description: 'Verdadeiro',
-          is_correct_answer: true,
-        },
-        {
-          public_id: 'false-2',
-          description: 'Falso',
-          is_correct_answer: false,
-        },
-      ],
-    },
-    {
-      id: 3,
-      public_id: 'quiz-question-3',
-      description: 'Questão 3 text',
-      correct_text_answer: 'correto',
-      time_limit: null,
-      type: QuestionType.TEXT,
-      options: [],
-    },
-  ],
-}
 
 export class SessionsManager {
   private static instance: SessionsManager
   active_sessions: Map<string, Session>
 
   constructor() {
-    console.info('SESSION MANAGER INIT')
     this.active_sessions = new Map()
-    const session = new Session(
-      {
-        id: 1,
-        public_id: '7be59af8-e47e-476f-b819-218a083f8434',
-        name: 'Instrutor teste',
-        role: UserRoles.INSTRUCTOR,
-      },
-      baseQuiz,
-    )
-    session.setDefaultCode()
-    this.active_sessions.set(session.getCode(), session)
   }
 
   static getInstance(): SessionsManager {
